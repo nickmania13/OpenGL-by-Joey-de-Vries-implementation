9df0c406275dfc993fcc7b0ade4e544f841c0511
Γάτος Νικόλαος <136273928+nickmania13@users.noreply.github.com>
2026-02-27 00:14:47 +0200

    Refactor to render a rectangle and add debug output
    Updated the rendering logic to draw a rectangle instead of a triangle and added error handling for OpenGL debug output.

--- main.cpp
+++ main.cpp
@@ -56,12 +56,13 @@ int main()
 	// Resize the viewport when the window is resized
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
-	// Definition of vertices
-	float vertices[] =
-	{
-		-0.5f, -0.5f, 0.0f, // left
-		 0.5f, -0.5f, 0.0f, // right
-		 0.0f,  0.5f, 0.0f  // top
+	// Create the element buffer object and copy the indices array in a buffer for OpenGL to use
+	float vertices[] = {
+		 0.5f, 0.5f, 0.0f,  // top right
+		-0.5f, 0.5f, 0.0f,  //top left
+		-0.5f, -0.5f, 0.0f, // bottom left
+		 0.5f, -0.5f, 0.0f  // bottom right
+
 	};
 	
 	// Generate a buffer and bind it to the GL_ARRAY_BUFFER target
@@ -128,17 +129,33 @@ int main()
 	// Telling OpenGL how to interpret the vertex data
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
+	
+	// Define the indices for the rectangle (two triangles)
+	unsigned int indices[] = {
+	0, 1, 3,  // first tringle
+	1, 2, 3   // second triangle
+	};
+	
+	// Generate the EBO object
+	unsigned int EBO;
+	glGenBuffers(1, &EBO);
 
 	// Generate a vertex array object and bind it
 	unsigned int VAO;
 	glGenVertexArrays(1, &VAO);
 	glBindVertexArray(VAO);
-	// Copy the vertices array in a buffer for OpenGL to use
+	// Copy the vertices array in a vertex buffer for OpenGL to use
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	// Copy the index array in an element buffer for OpenGL to use
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+	
 	// Set the vertex attributes pointers
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
+
+
 
 	// Render loop
 	while(!glfwWindowShouldClose(window))
@@ -151,10 +168,14 @@ int main()
 		glClear(GL_COLOR_BUFFER_BIT);
 
 
-		// Draw the triangle
+		// Draw the rectangle
 		glUseProgram(shaderProgram);
 		glBindVertexArray(VAO);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glDrawElements(GL_TRIANGLES, 6,GL_UNSIGNED_INT, 0);
+		glBindVertexArray(0);
+		glPolygonMode(GL_FRONT_AND_BACK, GL_FILL); // Wireframe mode
+
+
 
 		// check and call events and swap the buffers
 		glfwSwapBuffers(window);
@@ -185,4 +206,53 @@ void proccessInput(GLFWwindow* window)
 		glfwSetWindowShouldClose(window, true);
 	}
 
+}
+
+
+// Debugging
+void APIENTRY glDebugOutput(GLenum source,
+	GLenum type,
+	unsigned int id,
+	GLenum severity,
+	GLsizei length,
+	const char* message,
+	const void* userParam)
+{
+	// ignore non-significant error/warning codes
+	if (id == 131169 || id == 131185 || id == 131218 || id == 131204) return;
+
+	std::cout << "---------------" << std::endl;
+	std::cout << "Debug message (" << id << "): " << message << std::endl;
+
+	switch (source)
+	{
+	case GL_DEBUG_SOURCE_API:             std::cout << "Source: API"; break;
+	case GL_DEBUG_SOURCE_WINDOW_SYSTEM:   std::cout << "Source: Window System"; break;
+	case GL_DEBUG_SOURCE_SHADER_COMPILER: std::cout << "Source: Shader Compiler"; break;
+	case GL_DEBUG_SOURCE_THIRD_PARTY:     std::cout << "Source: Third Party"; break;
+	case GL_DEBUG_SOURCE_APPLICATION:     std::cout << "Source: Application"; break;
+	case GL_DEBUG_SOURCE_OTHER:           std::cout << "Source: Other"; break;
+	} std::cout << std::endl;
+
+	switch (type)
+	{
+	case GL_DEBUG_TYPE_ERROR:               std::cout << "Type: Error"; break;
+	case GL_DEBUG_TYPE_DEPRECATED_BEHAVIOR: std::cout << "Type: Deprecated Behaviour"; break;
+	case GL_DEBUG_TYPE_UNDEFINED_BEHAVIOR:  std::cout << "Type: Undefined Behaviour"; break;
+	case GL_DEBUG_TYPE_PORTABILITY:         std::cout << "Type: Portability"; break;
+	case GL_DEBUG_TYPE_PERFORMANCE:         std::cout << "Type: Performance"; break;
+	case GL_DEBUG_TYPE_MARKER:              std::cout << "Type: Marker"; break;
+	case GL_DEBUG_TYPE_PUSH_GROUP:          std::cout << "Type: Push Group"; break;
+	case GL_DEBUG_TYPE_POP_GROUP:           std::cout << "Type: Pop Group"; break;
+	case GL_DEBUG_TYPE_OTHER:               std::cout << "Type: Other"; break;
+	} std::cout << std::endl;
+
+	switch (severity)
+	{
+	case GL_DEBUG_SEVERITY_HIGH:         std::cout << "Severity: high"; break;
+	case GL_DEBUG_SEVERITY_MEDIUM:       std::cout << "Severity: medium"; break;
+	case GL_DEBUG_SEVERITY_LOW:          std::cout << "Severity: low"; break;
+	case GL_DEBUG_SEVERITY_NOTIFICATION: std::cout << "Severity: notification"; break;
+	} std::cout << std::endl;
+	std::cout << std::endl;
 }
