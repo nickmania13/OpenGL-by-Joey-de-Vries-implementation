eb0e98ab30511adc551338f40b2f7fde5d6e4247
Γάτος Νικόλαος <136273928+nickmania13@users.noreply.github.com>
2026-02-28 04:03:15 +0200

    Refactor shaders to include color attributes
    Updated vertex and fragment shaders to include color attributes and modified the rendering loop to use a changing color based on time.

--- main.cpp
+++ main.cpp
@@ -9,16 +9,20 @@ void proccessInput(GLFWwindow* window);
 
 const char* vertexShaderSource = "#version 330 core\n"
 "layout (location = 0) in vec3 aPos;\n"
+"layout (location = 1) in vec3 aColor;\n"
+"out vec3 ourColor;\n"
 "void main()\n"
 "{\n"
-"gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"gl_Position = vec4(aPos, 1.0);\n"
+"ourColor = aColor;\n"
 "}\0";
 
 const char* fragmentShaderSource = "#version 330 core\n"
 "out vec4 FragColor;\n"
+"in vec3 ourColor;\n"
 "void main()\n"
 "{\n"
-"FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+"FragColor = vec4(ourColor, 1.0);\n"
 "}\0";
 
 
@@ -58,10 +62,10 @@ int main()
 
 	// Create the element buffer object and copy the indices array in a buffer for OpenGL to use
 	float vertices[] = {
-		 0.5f, 0.5f, 0.0f,  // top right
-		-0.5f, 0.5f, 0.0f,  //top left
-		-0.5f, -0.5f, 0.0f, // bottom left
-		 0.5f, -0.5f, 0.0f  // bottom right
+		// positions      |     colors          
+		 0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f, // bottom right
+		-0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, // bottom left
+		 0.0f,  0.5f, 0.0f, 0.0f, 0.0f, 1.0f  // top
 
 	};
 	
@@ -127,13 +131,17 @@ int main()
 	glDeleteShader(fragmentShader);
 
 	// Telling OpenGL how to interpret the vertex data
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-	glEnableVertexAttribArray(0);
 	
-	// Define the indices for the rectangle (two triangles)
+	// position attribute
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+	// color attribute
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
+	
+	// Define the indices for the triangle
 	unsigned int indices[] = {
-	0, 1, 3,  // first tringle
-	1, 2, 3   // second triangle
+	0, 1, 2,  // first tringle
 	};
 	
 	// Generate the EBO object
@@ -151,10 +159,17 @@ int main()
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 	
-	// Set the vertex attributes pointers
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	// position attribute
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
 
+	// color attribute
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
+	
+	
+
+	
 
 
 	// Render loop
@@ -167,14 +182,23 @@ int main()
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
+		// activate the shader
+		glUseProgram(shaderProgram);
+		
+
+		// Using uniform to change the color of the rectangle
+		float timeValue = glfwGetTime();
+		float greenValue = (sin(timeValue) / 2.0f) + 0.5f;
+		int vertexColorLocation = glGetUniformLocation(shaderProgram, "ourColor");
+		
+		glUniform4f(vertexColorLocation, 0.5f, greenValue, 0.0f, 1.0f);  // Set the color of the rectangle to a changing green value
 
 		// Draw the rectangle
-		glUseProgram(shaderProgram);
 		glBindVertexArray(VAO);
-		glDrawElements(GL_TRIANGLES, 6,GL_UNSIGNED_INT, 0);
+		glDrawElements(GL_TRIANGLES, 3,GL_UNSIGNED_INT, 0);
 		glBindVertexArray(0);
 		glPolygonMode(GL_FRONT_AND_BACK, GL_FILL); // Wireframe mode
-
+		
 
 
 		// check and call events and swap the buffers
